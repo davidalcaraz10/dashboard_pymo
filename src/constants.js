a0ac6191012cb17bac01a58d2a356552a2206f9c
davidalcaraz10 <99300843+davidalcaraz10@users.noreply.github.com>
2024-02-02 22:01:50 -0600

    certificado ssl

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 // export const SERVER_HOST = import.meta.env.VITE_SERVER_HOST;
-export const SERVER_HOST = 'http://35.89.18.172:3330';
+export const SERVER_HOST = 'https://pymo-server.mensajesparasugloria.org';
